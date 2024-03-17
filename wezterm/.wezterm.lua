c34f788eff06eed624fa0c73fbaa22047f96a6d4
Richard Hao <lsxfhao@gmail.com>
2024-03-17 14:51:59 +0800

    Add wezterm basic config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+-- Appearance
+config.color_scheme = 'Tokyo Night'
+config.font_size = 13
+
+
+-- Key bindings
+config.keys = {
+  -- This will create a new split and run your default program inside it
+  {
+    key = '%',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  -- This will create a new split and run your default program inside it
+  {
+    key = '"',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
+
+return config
